306d25ef78aa7bb3e7ad6474641a71abe9e28002
Yzquile <deadnoct@gmail.com>
2023-07-18 23:32:12 +0800

    some additions and changes on Portfolio page

--- script.js
+++ script.js
@@ -6,47 +6,38 @@ menu.onclick = () => {
     navlist.classList.toggle('open');
 };
 
-const sr = ScrollReveal ({
+// ScrollReveal configuration
+const sr = ScrollReveal({
     distance: '65px',
     duration: 2600,
     delay: 450,
     reset: true
 });
 
-sr.reveal('.about',{delay:200, origin:'left'});
-sr.reveal('.my-image',{delay:450, origin:'right'});
+sr.reveal('.about', { delay: 200, origin: 'left' });
+sr.reveal('.my-image', { delay: 450, origin: 'right' });
 
-//portfolio
-const sliderContainer = document.querySelector('.slider-container');
-const sliderBtnLeft = document.querySelector('.slider-btn--left');
-const sliderBtnRight = document.querySelector('.slider-btn--right');
-const iframes = document.querySelectorAll('.slider-container iframe');
+// Portfolio Slider$(document).ready(function () {
+  const carousel = $(".carousel");
+  const videos = carousel.find(".video-wrapper");
+  const prevButton = $(".prev-button");
+  const nextButton = $(".next-button");
 
-let currentIndex = 0;
+  let currentVideoIndex = 0;
 
-sliderBtnLeft.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateSliderPosition();
-  }
-});
-
-sliderBtnRight.addEventListener('click', () => {
-  if (currentIndex < iframes.length - 1) {
-    currentIndex++;
-    updateSliderPosition();
-  }
-});
-
-function updateSliderPosition() {
-  const translation = -currentIndex * (280 + 10); // 280px width + 10px margin
-  sliderContainer.style.transform = `translateX(${translation}px)`;
-
-  // Remove and add current-video class to the current video iframe
-  iframes.forEach((iframe, index) => {
-    iframe.classList.remove('current-video');
-    if (index === currentIndex) {
-      iframe.classList.add('current-video');
-    }
+  prevButton.on("click", function () {
+    currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+    updateCarousel();
   });
-}
+
+  nextButton.on("click", function () {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    updateCarousel();
+  });
+
+  function updateCarousel() {
+    const translateXValue = -currentVideoIndex * 100;
+    carousel.css("transform", `translateX(${translateXValue}%)`);
+  }
+
+  
