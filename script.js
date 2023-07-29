ac20e5d412970a5980544eaade9766aaf8fda838
Yzquile <deadnoct@gmail.com>
2023-07-29 16:59:19 +0800

    save point for checkbox dropdown menu

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+const menuIcon = document.getElementById('menu-item');
+const header = document.querySelector('.header');
+const navbar = document.querySelector('.navbar');
+const links = document.querySelector('.links');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-};
+menuIcon.addEventListener('click', function() {
+    // Toggle the menu-open class on the header element
+    header.classList.toggle('menu-open');
 
+    // If the menu-open class is present, display the navbar and links in a column
+    if (header.classList.contains('menu-open')) {
+        navbar.style.display = 'block';
+        links.style.display = 'block';
+    } else {
+        // If the menu-open class is not present, hide the navbar and links
+        navbar.style.display = 'none';
+        links.style.display = 'none';
+    }
+});
